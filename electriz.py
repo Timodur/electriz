9a3b6e61e04979fc2675751a5d51029615f37462
Timothé Durand <timothedurand2004@gmail.com>
2026-01-30 04:00:45 +0100

    refactor :  code structure for improved readability and maintainability

--- electriz.py
+++ electriz.py
@@ -1,5 +1,8 @@
 import chess
 import random
+import chess.svg
+from IPython.display import SVG
+
 
 def choisir_coup_aleatoire(board):
     """Choisit un coup aléatoire parmi les coups légaux"""
@@ -10,13 +13,14 @@ def jouer_partie():
     board = chess.Board()
     
     while not board.is_game_over():
-        print(f"\n{board}\n")
         
         coup = choisir_coup_aleatoire(board)
-        print(f"Coup joué : {coup}")
+        print(coup)
+        SVG(chess.svg.board(board=board, lastmove=coup))
         board.push(coup)
     
     print(f"\nRésultat : {board.result()}")
-
+    print(f"\nFEN finale : {board.fen()}")
+    SVG(chess.svg.board(board=board, size=400))
 if __name__ == "__main__":
     jouer_partie()
